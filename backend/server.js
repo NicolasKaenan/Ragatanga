2503652944857bf860291720ced0be733213388b
Kaenan <nicolaskaenandev@gmail.com>
2024-11-27 16:43:03 -0300

    :)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const cors = require('cors')
+const jwt = require("jsonwebtoken"); // For generating JWTs
 
-
+// Replace this with a strong secret key and store it securely
 const JWT_SECRET = "your_jwt_secret_key";
-
 
 const connection = mysql.createPool({
     host: 'localhost',
@@ -19,11 +18,12 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'http://127.0.0.1:5500', // Indica quem pode se conectar 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true, // Permite cookies e cabeçalhos de autorização
+    allowedHeaders: ['Content-Type', 'Authorization'] // Liste os cabeçalhos que você espera receber
 }));
+
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -79,7 +79,6 @@ app.post("/register", async (req, res) => {
 // Login Route
 app.post("/login", async (req, res) => {
     try {
-        console.log("ola")
         const { email, password } = req.body;
 
         // Retrieve the user from the database
@@ -120,10 +119,71 @@ app.post("/login", async (req, res) => {
     }
 });
 
+app.get("/questions", authenticateToken, async (req, res) => { // tem que adicionar matérias aqui 
+    try {
+        const query = `
+            SELECT 
+                questions.title,
+                questions.question_description,
+                questions.closed,
+                questions.main_response,
+                questions.createdAt,
+                COUNT(relevanceVote.id) AS relevantVotes
+            FROM 
+                questions
+            LEFT JOIN 
+                relevanceVote 
+            ON 
+                questions.id = relevanceVote.question_id
+            GROUP BY 
+                questions.id;
+        `;
+        const [response] = await connection.query(query); 
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching questions." });
+    }
+});
+
+
+app.get("/myQuestions", authenticateToken, async (req, res) => { // tem que adicionar matérias aqui 
+
+    try {
+        const query = `
+            SELECT 
+                questions.title,
+                questions.question_description,
+                questions.closed,
+                questions.main_response,
+                questions.createdAt,
+                COUNT(relevanceVote.id) AS relevantVotes
+            FROM 
+                questions
+            LEFT JOIN 
+                relevanceVote 
+            ON 
+                questions.id = relevanceVote.question_id
+            WHERE
+            questions.creator_id = ?
+
+            GROUP BY 
+                questions.id;
+        `;
+        const [response] = await connection.query(query,req.user.id); 
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching questions." });
+    }
+});
+
+
+
 app.post("/question",authenticateToken, async(req,res) =>{
-    const { title,question_description } = req.body;
+    const { title,subtitle,question_description,subjects} = req.body;
     try{
-        await connection.query("INSERT INTO questions (title, question_description, creator_id) VALUES (?,?,?)", [title,question_description,req.user.id])
+        await connection.query("INSERT INTO questions (title, subtitle, question_description, creator_id,subjects) VALUES (?,?,?,?,?)", [title,subtitle,question_description,req.user.id,JSON.stringify(subjects)])
         res.status(200).json({
             message: "Acess granted",
             user : req.user
@@ -147,6 +207,33 @@ app.post("/answer/:question_id",authenticateToken,async(req,res) =>{
         return res.status(401).json({ error: err});
     }
 })  
+
+app.post("/voteRelevance/:question_id",authenticateToken,async(req,res) =>{
+    const question_id = req.params.question_id;
+    const user_id = req.user.id;
+    try{
+        await connection.query("INSERT INTO relevanceVote (question_id,users_id) VALUES (?, ?)", [question_id,user_id])
+        return res.status(200).json({message : "voted answered" });
+    }
+    catch(err){
+        return res.status(401).json({ error: err});
+    }
+})  
+
+app.post("/upvote/:answer_id",authenticateToken,async(req,res) =>{
+ 
+    const answer_id = req.params.answer_id;
+    const user_id = req.user.id;
+    try{
+        await connection.query("INSERT INTO upvotes (answers_id,users_id) VALUES (?, ?)", [answer_id,user_id])
+        return res.status(200).json({message : "voted answered" });
+    }
+    catch(err){
+        return res.status(401).json({ error: err});
+    }
+})  
+
+
 // Protected Route Example
 app.get("/profile", async (req, res) => {
     const authHeader = req.headers.authorization;
